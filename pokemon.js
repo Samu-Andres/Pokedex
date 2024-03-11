68eb6e7cdc9abdd56f094ccdc4645aca906e6b74
samuel <111392600+Samu-Andres@users.noreply.github.com>
2024-03-11 12:25:47 -0300

    Actualizado

--- pokemon.js
+++ pokemon.js
@@ -1,4 +1,4 @@
-const MAX_POKEMON = 650;
+const MAX_POKEMON = 150;
 const listWrapper = document.querySelector(".list-wrapper");
 const searchInput = document.querySelector("#search-input");
 const numberFilter = document.querySelector("#number");
